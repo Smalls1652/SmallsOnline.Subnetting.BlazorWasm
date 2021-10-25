cfde99c2b6aac4e62afe8278bcc067a0b51439be
Timothy Small <smalls.online@icloud.com>
2021-10-25 10:49:24 -0400

    Initial commit

---
+++ src/SmallsOnline.Subnetting.BlazorWasm/models/SubnetInput.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmallsOnline.Subnetting.BlazorWasm.Models
+{
+    public class SubnetInput
+    {
+        [Required]
+        public string SubnetString { get; set; }
+    }
+}
